cf46c6a73961ccaa7d6bf2b7899c30b700f0a7b3
Kocsis Richárd <ricsik52@gmail.com>
2021-04-01 15:44:35 +0200

    favourite_view

---
+++ app/src/main/java/com/example/anime_list/DeatilsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.anime_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeatilsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deatils)
+    }
+}
---
+++ app/src/main/java/com/example/anime_list/FavouriteActivity.kt
@@ -0,0 +1,57 @@
+package com.example.anime_list
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FavouriteActivity: AppCompatActivity() {
+
+    private lateinit var adapter:FavouriteListAdapter
+    private lateinit var recyclerView:RecyclerView
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var toolBar:Toolbar
+
+    private fun initRecyclerview(){
+        adapter= FavouriteListAdapter()
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById<RecyclerView>(R.id.RecyclerView)
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = adapter
+    }
+    fun loadItems(){
+        adapter.initList(listOf("ALMA","KÖRTE"))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourites)
+        initRecyclerview()
+        loadItems()
+        toolBar=findViewById(R.id.toolbar)
+        setSupportActionBar(toolBar)
+        toolBar.inflateMenu(R.menu.menu_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_animelist -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
